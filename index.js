cc591788a5dba4f53b00c7f1192402700cd91968
Mikael Nelson <mikaelnelson@gmail.com>
2017-11-28 19:45:32 -0600

    Very basic thermostat service outline

--- index.js
+++ index.js
@@ -1,57 +1,102 @@
 var Service, Characteristic;
-var switchState = false;
 
-module.exports = function( homebridge ) {
-    console.log("Homebridge!");
-    
+module.exports = function( homebridge ) {    
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     
-    homebridge.registerAccessory("switch-plugin", "MySwitch", mySwitch);
+    homebridge.registerAccessory("homebridge-kumo", "Kumo", Kumo);
 };
 
-mySwitch.prototype = {
-  getServices: function() {
-      let informationService = new Service.AccessoryInformation();
-      
-      informationService
-        .setCharacteristic( Characteristic.Manufacturer, "Switch Manu")
-        .setCharacteristic( Characteristic.Model, "Switch Model")
-        .setCharacteristic( Characteristic.SerialNumber, "1234");
-      
-      let switchService = new Service.Switch("My Switch");
-      
-      switchService
-        .getCharacteristic(Characteristic.On)
-            .on('get', this.getSwitchOnCharacteristic.bind(this))
-            .on('set', this.setSwitchOnCharacteristic.bind(this));
-      
-      this.informationService = informationService;
-      this.switchService = switchService;
-      
-      return [informationService, switchService];
-  },
-    
-  getSwitchOnCharacteristic: function( next ) {
-      const me = this;
-      console.log("Get Switch State!");
-        
-      return next( null, switchState );
-  },
-    
-  setSwitchOnCharacteristic: function(on, next) {
-      const me = this;
-      console.log("Set Switch State: " + on);
-        
-      switchState = on;
-      
-      return next();
-  }
-};
-
-function mySwitch(log, config) {
+function Kumo(log, config) {
     this.log = log;
     //geturl
     //posturl
 }
 
+Kumo.prototype = {
+    identity: function(callback) {
+        console.log("Request Identity!");
+        callback(null);
+    },
+    
+    getCurrentHeatingCoolingState: function(callback) {
+        console.log("Get Current Heating/Cooling State!");
+        
+        //Perform Callback To Return Result
+        callback( null, Characteristic.TargetHeatingCoolingState.AUTO);
+    },
+    
+    getTargetHeatingCoolingState: function(callback) {
+        console.log("Get Target Heating/Cooling State!");
+        
+        callback( null, Characteristic.TargetHeatingCoolingState.AUTO);
+    },
+    
+    getCurrentTemperature: function(callback) {
+        console.log("Get Current Temperature!");
+        
+        callback( null, 72 );
+    },
+    
+    getTargetTemperature: function(callback) {
+        console.log("Get Target Temperature!");
+        
+        callback( null, 72);
+    },
+    
+    setTargetTemperature: function(value, callback) {
+        console.log("Set Target Temperature: " + value );
+        
+        callback(null);
+    },
+    
+    getTemperatureDisplayUnits: function(callback) {
+        console.log("Get Temperature Display Units!");
+        
+        callback( null, Characteristic.TemperatureDisplayUnits.CELSIUS);
+    },
+    
+    setTemperatureDisplayUnits: function(value, callback) {
+        console.log("Set Temperature Display Units!");
+        
+        callback( null );
+    },
+    
+    getServices: function() {
+        let informationService = new Service.AccessoryInformation();
+
+        informationService
+            .setCharacteristic( Characteristic.Manufacturer, "Kumo")
+            .setCharacteristic( Characteristic.Model, "Kumo Model")
+            .setCharacteristic( Characteristic.SerialNumber, "1234");
+
+        let thermostatService = new Service.Thermostat("Kumo");
+
+        thermostatService
+            .getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+                .on('get', this.getCurrentHeatingCoolingState.bind(this));
+
+        thermostatService
+            .getCharacteristic(Characteristic.TargetHeatingCoolingState)
+                .on('get', this.getTargetHeatingCoolingState.bind(this));
+        
+        thermostatService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+                .on('get', this.getCurrentTemperature.bind(this));
+        
+        thermostatService
+            .getCharacteristic(Characteristic.TargetTemperature)
+                .on('get', this.getTargetTemperature.bind(this))
+                .on('set', this.setTargetTemperature.bind(this));
+        
+        thermostatService
+            .getCharacteristic(Characteristic.TemperatureDisplayUnits)
+                .on('get', this.getTemperatureDisplayUnits.bind(this))
+                .on('set', this.setTemperatureDisplayUnits.bind(this));    
+        
+        this.informationService = informationService;
+        this.thermostatService = thermostatService;
+
+        return [informationService, thermostatService];
+    }
+};
